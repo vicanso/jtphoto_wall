d6ad7afe248031455ebf89fc06494a5d29941c28
vicanso <vicansocanbico@gmail.com>
2014-01-12 20:47:37 +0800

    commit

--- jtphoto_wall.coffee
+++ jtphoto_wall.coffee
@@ -1,54 +1,53 @@
 "use strict"
-moduleName = 'jtphoto_wall'
-module = GLOBAL_MODULES[moduleName] = 
-  id : moduleName
-exports = module.exports = {}
 
-require 'jquery'
-require 'underscore'
-_ = window._
-$ = window.jQuery
 
-randomAnimate = (items, outerWidth, outerHeight) ->
-  animateOptionsList = [
-    {
-      left : -outerWidth
-    }
-    {
-      left : outerWidth
-    }
-    {
-      top : outerHeight
-    }
-    {
-      top : -outerHeight
-    }
-  ]
-  animateItems = _.sample items, 10
-  max = animateOptionsList.length
-  _.each animateItems, (item, i) ->
-    item = $ item
-    if !item.data 'hidden'
-      tmpAnimateOptions = animateOptionsList[_.random(0, max)]
-      item.data 'hidden', true
-    else
-      item.data 'hidden', false
-      tmpAnimateOptions = 
-        left : 0
-        top : 0
-    item.delay(i * 100).animate tmpAnimateOptions
 
-exports.init = (items, interval = 6000) ->
-  lastItem = items.filter ':last-child'
-  outerWidth = lastItem.outerWidth()
-  outerHeight = lastItem.outerHeight()
-  timer = null
-  start = ->
-    clearTimeout timer if timer
-    timer = setTimeout ->
-      timer = null
-      randomAnimate items, outerWidth, outerHeight
-      start()
-    , 6 * 1000
-    null
-  start()
+define 'jtPhotoWall', ['jquery', 'underscore'], (require, exports) ->
+  $ = require 'jquery'
+  _ = require 'underscore'
+  randomAnimate = (items, outerWidth, outerHeight) ->
+    animateOptionsList = [
+      {
+        left : -outerWidth
+      }
+      {
+        left : outerWidth
+      }
+      {
+        top : outerHeight
+      }
+      {
+        top : -outerHeight
+      }
+    ]
+    animateItems = _.sample items, 10
+    max = animateOptionsList.length
+    _.each animateItems, (item, i) ->
+      item = $ item
+      if !item.data 'hidden'
+        tmpAnimateOptions = animateOptionsList[_.random(0, max)]
+        item.data 'hidden', true
+      else
+        item.data 'hidden', false
+        tmpAnimateOptions = 
+          left : 0
+          top : 0
+      item.delay(i * 100).animate tmpAnimateOptions
+
+  init = (items, interval = 6000) ->
+    lastItem = items.filter ':last-child'
+    outerWidth = lastItem.outerWidth()
+    outerHeight = lastItem.outerHeight()
+    timer = null
+    start = ->
+      clearTimeout timer if timer
+      timer = setTimeout ->
+        timer = null
+        randomAnimate items, outerWidth, outerHeight
+        start()
+      , 6 * 1000
+      null
+    start()
+
+  exports.init = init
+  return
--- jtphoto_wall.js
+++ jtphoto_wall.js
@@ -1,77 +1,64 @@
 (function() {
   "use strict";
 
-  var $, exports, module, moduleName, randomAnimate, _;
-
-  moduleName = 'jtphoto_wall';
-
-  module = GLOBAL_MODULES[moduleName] = {
-    id: moduleName
-  };
-
-  exports = module.exports = {};
-
-  require('jquery');
-
-  require('underscore');
-
-  _ = window._;
-
-  $ = window.jQuery;
-
-  randomAnimate = function(items, outerWidth, outerHeight) {
-    var animateItems, animateOptionsList, max;
-    animateOptionsList = [
-      {
-        left: -outerWidth
-      }, {
-        left: outerWidth
-      }, {
-        top: outerHeight
-      }, {
-        top: -outerHeight
-      }
-    ];
-    animateItems = _.sample(items, 10);
-    max = animateOptionsList.length;
-    return _.each(animateItems, function(item, i) {
-      var tmpAnimateOptions;
-      item = $(item);
-      if (!item.data('hidden')) {
-        tmpAnimateOptions = animateOptionsList[_.random(0, max)];
-        item.data('hidden', true);
-      } else {
-        item.data('hidden', false);
-        tmpAnimateOptions = {
-          left: 0,
-          top: 0
-        };
-      }
-      return item.delay(i * 100).animate(tmpAnimateOptions);
-    });
-  };
-
-  exports.init = function(items, interval) {
-    var lastItem, outerHeight, outerWidth, start, timer;
-    if (interval == null) {
-      interval = 6000;
-    }
-    lastItem = items.filter(':last-child');
-    outerWidth = lastItem.outerWidth();
-    outerHeight = lastItem.outerHeight();
-    timer = null;
-    start = function() {
-      if (timer) {
-        clearTimeout(timer);
-      }
-      timer = setTimeout(function() {
-        timer = null;
-        randomAnimate(items, outerWidth, outerHeight);
-        return start();
-      }, 6 * 1000);
-      return null;
+  define('jtPhotoWall', ['jquery', 'underscore'], function(require, exports) {
+    var $, init, randomAnimate, _;
+    $ = require('jquery');
+    _ = require('underscore');
+    randomAnimate = function(items, outerWidth, outerHeight) {
+      var animateItems, animateOptionsList, max;
+      animateOptionsList = [
+        {
+          left: -outerWidth
+        }, {
+          left: outerWidth
+        }, {
+          top: outerHeight
+        }, {
+          top: -outerHeight
+        }
+      ];
+      animateItems = _.sample(items, 10);
+      max = animateOptionsList.length;
+      return _.each(animateItems, function(item, i) {
+        var tmpAnimateOptions;
+        item = $(item);
+        if (!item.data('hidden')) {
+          tmpAnimateOptions = animateOptionsList[_.random(0, max)];
+          item.data('hidden', true);
+        } else {
+          item.data('hidden', false);
+          tmpAnimateOptions = {
+            left: 0,
+            top: 0
+          };
+        }
+        return item.delay(i * 100).animate(tmpAnimateOptions);
+      });
     };
-    return start();
-  };
+    init = function(items, interval) {
+      var lastItem, outerHeight, outerWidth, start, timer;
+      if (interval == null) {
+        interval = 6000;
+      }
+      lastItem = items.filter(':last-child');
+      outerWidth = lastItem.outerWidth();
+      outerHeight = lastItem.outerHeight();
+      timer = null;
+      start = function() {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+          timer = null;
+          randomAnimate(items, outerWidth, outerHeight);
+          return start();
+        }, 6 * 1000);
+        return null;
+      };
+      return start();
+    };
+    exports.init = init;
+  });
 
 }).call(this);
